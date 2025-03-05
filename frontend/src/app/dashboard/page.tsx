6cf7c30934724109ae2408e0fc3b484f18bcf683
vincentvnoord <vannoord.vincent@gmail.com>
2025-03-05 15:26:16 +0100

    Dashboard layout

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { getUserData } from "./_actions";
+import { ChevronRight, PlusIcon, UserIcon } from "lucide-react";
 
 export default async function DashBoardPage() {
     const cookieStore = await cookies();
@@ -22,8 +23,63 @@ export default async function DashBoardPage() {
     const { id, email } = res;
 
     return (
-        <div className="flex items-center justify-center h-dvh">
-            <h1>Welcome back {email}, ur id is {id}</h1>
+        <div className="flex flex-col h-dvh relative">
+            <header className="bg-primary top-0 left-0 z-20 sticky text-white p-3 px-6 flex justify-between items-center rounded-b-[30px]">
+                <h1 className="font-bold text-2xl">Your Quizzes</h1>
+                <UserIcon size={28} />
+            </header>
+
+            <div className="flex flex-col p-4 gap-2">
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+                <ListedQuiz />
+            </div>
+
+            <div className="fixed bottom-0 right-0 p-3 pb-6">
+                <button className="bg-primary p-3 rounded-xl shadow-lg">
+                    <PlusIcon className="text-white" size={32} strokeWidth={1} />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+const ListedQuiz = () => {
+
+    return (
+        <div className="bg-white rounded-xl p-3 flex justify-between items-center">
+            <div className="flex flex-col">
+                <p className="font-semibold">Quiz Title</p>
+                <p className="text-sm opacity-50">10 questions</p>
+            </div>
+
+            <ChevronRight className="opacity-50" size={32} />
         </div>
     )
 }
