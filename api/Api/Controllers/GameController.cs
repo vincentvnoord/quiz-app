7a81ecb1a77a420c6511b938e5d5b602898aaa9c
vincentvnoord <vannoord.vincent@gmail.com>
2025-03-26 17:55:15 +0100

    Added response types in join route

--- api/Api/Controllers/GameController.cs
+++ api/Api/Controllers/GameController.cs
@@ -69,6 +69,10 @@ namespace Api.Controllers
         }
 
         [HttpPost("join")]
+        [ProducesResponseType(200, Type = typeof(JoinGameResponse))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
         public IActionResult Join([FromBody] JoinGameRequest request)
         {
             if (!ModelState.IsValid)
